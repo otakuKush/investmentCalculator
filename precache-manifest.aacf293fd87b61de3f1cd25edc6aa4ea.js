28d7ed502fdbb43c6b8ec40ab523e8a46269b1dc
Kushagra Porwal <kushagra.porwal@realitypremedia.onmicrosoft.com>
2020-05-18 22:11:51 +0530

    Updates

---
+++ precache-manifest.aacf293fd87b61de3f1cd25edc6aa4ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8561183b771053a03141bc8ed5debc25",
+    "url": "/investmentCalculator/index.html"
+  },
+  {
+    "revision": "f1ddf02532137f80f503",
+    "url": "/investmentCalculator/static/css/2.7c29dcd6.chunk.css"
+  },
+  {
+    "revision": "525833f60c3e10aea710",
+    "url": "/investmentCalculator/static/css/main.021c415e.chunk.css"
+  },
+  {
+    "revision": "f1ddf02532137f80f503",
+    "url": "/investmentCalculator/static/js/2.32343c40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/investmentCalculator/static/js/2.32343c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525833f60c3e10aea710",
+    "url": "/investmentCalculator/static/js/main.0657a29d.chunk.js"
+  },
+  {
+    "revision": "91cf8a842b3093888e93",
+    "url": "/investmentCalculator/static/js/runtime-main.8006a293.js"
+  }
+]);
